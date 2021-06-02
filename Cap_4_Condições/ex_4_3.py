32ee79d8588ce160069882cf468df932a4b0ff1f
Pedro Neto <pedroceciliocn@gmail.com>
2021-06-02 18:30:38 -0300

    up

--- Cap_4_Condições/ex_4_3.py
+++ Cap_4_Condições/ex_4_3.py
@@ -10,18 +10,16 @@ if n_1 > n_2 and n_1 > n_3:
         menor = n_2
     elif n_3 < n_2:
         menor = n_3
-    print(f"{maior} é o maior número e {menor} é o menor.")
 if n_2 > n_1 and n_2 > n_3:
     maior = n_2
     if n_3 < n_1:
         menor = n_3
     elif n_1 < n_3:
         menor = n_1
-    print(f"{maior} é o maior número e {menor} é o menor.")
 if n_3 > n_1 and n_3 > n_2:
     maior = n_3
     if n_2 < n_1:
         menor = n_2
     elif n_1 < n_2:
         menor = n_1
-    print(f"{maior} é o maior número e {menor} é o menor.")
+print(f"{maior} é o maior número e {menor} é o menor.")
--- Cap_4_Condições/ex_4_3_alt.py
+++ Cap_4_Condições/ex_4_3_alt.py
@@ -12,4 +12,18 @@ if n_2 < n_3 and n_2 < n_1:
     menor = n_2
 if n_3 < n_2 and n_3 < n_1:
     menor = n_3
-print(f"{maior} é o maior número e {menor} é o menor.")
+print(f"O maior numero é {maior} e o menor é {menor}.")
+
+
+#############Questão do slide da aula###########################################
+n_1 = int(input("De o primeiro numero: "))
+n_2 = int(input("De o segundo numero: "))
+n_3 = int(input("De o terceiro numero: "))
+
+menor = n_1
+
+if n_2 <= n_3 and n_2 <= n_1:
+    menor = n_2
+if n_3 <= n_2 and n_3 <= n_1:
+    menor = n_3
+print(f"O menor numero é {menor}.")
